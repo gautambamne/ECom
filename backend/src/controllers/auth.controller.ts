eda10d369e9138ac56a3c636bca97da47d2b0a18
gautambamne <gautam2003b@gmail.com>
2025-08-31 19:39:31 +0530

    Login controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,11 +1,21 @@
 import { ApiError } from "../advices/ApiError";
 import { ApiResponse } from "../advices/ApiResponse";
-import { RegistrationSchema } from "../schema/auth.schema"
+import { LoginSchema, RegistrationSchema } from "../schema/auth.schema"
 import asyncHandler from "../utils/asyncHandler"
 import { zodErrorFormatter } from "../utils/format-validation-error";
 
 export const RegisterController = asyncHandler(async (req, res)=>{
   const result = RegistrationSchema.safeParse(req.body)
+  if(!result.success){
+        throw new ApiError(400, "validation Error", zodErrorFormatter(result.error))
+  }
+
+  return res.json(
+    new ApiResponse(result.data)
+  )
+})
+export const LoginController = asyncHandler(async (req, res)=>{
+  const result = LoginSchema.safeParse(req.body)
   if(!result.success){
         throw new ApiError(400, "validation Error", zodErrorFormatter(result.error))
   }
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { RegisterController } from "../controllers/auth.controller";
+import { LoginController, RegisterController } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
 authRouter.post('/register', RegisterController);
+authRouter.post('/login', LoginController);
 
 export default authRouter;
