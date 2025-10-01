7c3053e57648a30c9cfedc9dbb6bc0b436035cbd
gautambamne <gautam2003b@gmail.com>
2025-10-01 12:01:46 +0530

    design the login and register page

--- frontend/src/components/base/navbar/navbar.tsx
+++ frontend/src/components/base/navbar/navbar.tsx
@@ -70,14 +70,14 @@ export function Navbar({ menuState, setMenuState }: { menuState: boolean, setMen
                 asChild
                 variant="outline"
                 size="sm">
-                <Link href="#">
+                <Link href="/login">
                   <span>Login</span>
                 </Link>
               </Button>
               <Button
                 asChild
                 size="sm">
-                <Link href="#">
+                <Link href="/register">
                   <span>Sign Up</span>
                 </Link>
               </Button>
